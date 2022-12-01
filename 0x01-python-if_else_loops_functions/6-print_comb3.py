a684451cdf85600c133fe31ba612b02e72e20a95
lionel72 <yamkamlionel@yahoo.fr>
2022-12-01 02:00:39 -0800

    print digit

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 9):
     for j in range(i+1, 10):
-        if i == 8 and j == 9:
-            print("{0}{1}".format(i, j))
-        print("{0}{1}".format(i, j), end=", ")
+        if i != 8:
+            print("{0}{1}".format(i, j), end=", ")
+print("{0}{1}".format(i, j))
