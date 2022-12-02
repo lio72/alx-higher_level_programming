b3d355ed27b6bb21cfbd87957983f061e46390f6
lionel72 <yamkamlionel@yahoo.fr>
2022-12-01 17:32:58 -0800

    calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -22,3 +22,4 @@ if __name__ == '__main__':
             print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
         elif sys.argv[2] == "/":
             print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
+            exit(0)
