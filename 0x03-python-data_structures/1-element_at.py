aaa2b995753b0c83cc16b83a38c19e6a50e1e4ac
lionel72 <yamkamlionel@yahoo.fr>
2022-12-03 00:40:27 -0800

    print element

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,5 +1,6 @@
-#:/usr/bin/python3
+#!/usr/bin/python3
 def element_at(my_list, idx):
+    """print element of list with index"""
     if idx in list(range(0, len(my_list))):
         return my_list[idx]
     else:
