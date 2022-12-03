7b66b9a7e0fdd6cea62f9ab19ffa2a562eb587a9
lionel72 <yamkamlionel@yahoo.fr>
2022-12-03 03:09:20 -0800

    replace without change

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    """ replace element without change list"""
+    if idx in list(range(0, len(my_list))):
+        new_list = my_list[:]
+        new_list[idx] = element
+        return new_list
+    return my_list
