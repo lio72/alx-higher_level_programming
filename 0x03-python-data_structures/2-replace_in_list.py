838d8c7abd2b63edf922f25f21b3be31420bbac2
lionel72 <yamkamlionel@yahoo.fr>
2022-12-03 01:42:15 -0800

    replace value

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,7 +2,5 @@
 def replace_in_list(my_list, idx, element):
     """Replace value in list"""
     if idx in list(range(0, len(my_list))):
-        my_list = my_list.insert(idx, element)
-        return my_list
-    else:
-        return my_list
+        my_list[idx] = element
+    return my_list
