066deadd46b98845a7124a23139069ffb3602221
lionel72 <yamkamlionel@yahoo.fr>
2022-12-03 02:24:26 -0800

    print reserved

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     """ Print reserved list"""
-    i = len(my_list) - 1
-    while i >= 0:
-        print("{:d}".format(my_list[i]))
+    i = len(my_list)
+    while i > 0:
         i = i - 1
+        print("{:d}".format(my_list[i]))
