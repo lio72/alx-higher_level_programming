e7ee0b6366282e63ad04e73a211f956e3f4cb076
lionel72 <yamkamlionel@yahoo.fr>
2022-12-03 03:34:47 -0800

    remove char in string

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    """remove specifie character in string"""
+    if my_string:
+        i = 0
+        for s in my_string:
+            if s in "sS":
+                del my_string[i]
+            i = i + 1
+        return my_string
