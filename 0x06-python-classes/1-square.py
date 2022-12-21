ed8671f3bbce97679a1a8891a1bc139e62e4e4b5
lionel72 <yamkamlionel@yahoo.fr>
2022-12-21 15:07:53 -0800

    1 -square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" we define our class with one attribut"""
+
+
+class Square:
+    """our class is initialisez with size"""
+
+    def __init__(self, size):
+        self.size = size
