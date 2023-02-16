7924e7da5c1f1f396a7040dda1b5d51456f52b23
lionel72 <yamkamlionel@yahoo.fr>
2023-02-16 08:33:04 -0800

    display

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,3 @@
--- display all items of our tables
-SELECT score, name FROM second_table;
+-- display all items of our tables.
+SELECT score, name FROM second_table
+ORDER BY score DESC;
--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,4 @@
 -- display items of our table using condition.
 SELECT score, name FROM second_table WHERE score >= 10;
+ORDER BY score DESC;
+
